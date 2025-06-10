4b75a33f912f5aeee726455495d5f1504432325d
haicao <caothanhhai18@gmail.com>
2025-06-10 16:21:03 +0700

    1) Support Qt Quick Controls Configuration File 2) Set QML_IMPORT_PATH to import qml module

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
-
+#include "app_environment.h"
+#include "import_qml_plugins.h"
 int main(int argc, char *argv[])
 {
+    set_qt_environment();
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
